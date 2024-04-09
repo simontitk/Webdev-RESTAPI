0e092f96cc17811c6278ee9973d3afc4b6d9b7b0
najakjen <najj@itu.dk>
2024-04-09 09:26:38 +0200

    create productsRouter and add service

--- services/service.js
+++ services/service.js
@@ -106,7 +106,117 @@ class ordersService {
 }
 
 class productsService {
+    async getAllProducts() {
+        try {
+            const products = await prisma.products.findMany()
+            return products
+        } catch (err) {
+            console.error(err)
+            throw new Error(`Error fetching products from database: ${err.message}`)
+        }
+    }
 
+    async createProduct(name, brand, description, picture_uri, volume, amount, rating, price) {
+        try {
+            const product = await prisma.products.create({
+                data: {
+                    name: name,
+                    brand: brand,
+                    description: description,
+                    picture_uri: picture_uri,
+                    volume: volume, 
+                    amount: amount, 
+                    rating: rating, 
+                    price: price,
+                }
+            })
+            return product
+        } catch (err) {
+            console.error(err)
+            throw new Error(`Error adding product to database: ${err.message}`)
+        }
+    }
+
+    async updateAllProducts(name, brand, description, picture_uri, volume, amount, rating, price) {
+        try {
+            const product = await prisma.products.update({
+                data: {
+                    name: name,
+                    brand: brand,
+                    description: description,
+                    picture_uri: picture_uri,
+                    volume: volume, 
+                    amount: amount, 
+                    rating: rating, 
+                    price: price,
+                }
+            })
+            return product
+        } catch (err) {
+            console.error(err)
+            throw new Error(`Error updating products: ${err.message}`)
+        }
+    }
+
+    async deleteAllProducts() {
+        try {
+            await prisma.products.deleteMany()
+        } catch (err) {
+            console.error(err)
+            throw new Error(`Error deleting all products: ${err.message}`)
+        }
+    }
+
+    async getProduct(productId) {
+        try {
+            const product = await prisma.products.findMany({
+                where: {
+                    id: productId
+                }
+            })
+            return product
+        } catch (err) {
+            console.error(err)
+            throw new Error(`Error getting product from database: ${err.message}`)
+        }
+    }
+
+    async updateProduct(productId, name, brand, description, picture_uri, volume, amount, rating, price) {
+        try {
+            const product = await prisma.products.update({
+                where: {
+                    id: productId
+                },
+                data: {
+                    name: name,
+                    brand: brand,
+                    description: description,
+                    picture_uri: picture_uri,
+                    volume: volume, 
+                    amount: amount, 
+                    rating: rating, 
+                    price: price,
+                }
+            })
+            return product
+        } catch (err) {
+            console.error(err)
+            throw new Error(`Error updating product: ${err.message}`)
+        }
+    }
+
+    async deleteProduct(productId) {
+        try {
+            await prisma.products.delete({
+                where: {
+                    id: productId
+                }
+            })
+        } catch (err) {
+            console.error(err)
+            throw new Error(`Error deleting product: ${err.message}`)
+        }
+    }
 }
 
 class usersService {
