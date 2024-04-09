94ebc8e25da9779ac87d05dc04dd83cf07701e36
simontitk <simontitk@gmail.com>
2024-04-09 11:46:33 +0200

    add get endpoints for orders, change imports/exports

--- services/service.js
+++ services/service.js
@@ -102,7 +102,41 @@ class cartsService {
 }
 
 class ordersService {
+    async getAllOrders() {
+        try {
+            const orders = await prisma.orders.findMany({
+                include: {
+                    Order_items: {include: {product: true}},
+                    user: true
+                }
+            });
+            return orders;
+        }
+        catch (err) {
+            console.error(err);
+            throw new Error(`Error fetching orders from database: ${err.message}`)
 
+        }
+    }
+
+    async getOrder(orderId) {
+        try {
+            const order = await prisma.orders.findUnique({
+                where: {
+                    id: orderId
+                },
+                include: {
+                    Order_items: {include: {product: true}},
+                    user: true
+                }
+            });
+            return order;
+        } 
+        catch (err) {
+            console.error(err);
+            throw new Error(`Error fetching order from database: ${err.message}`)
+        }
+    }
 }
 
 class productsService {
@@ -113,4 +147,4 @@ class usersService {
 
 }
 
-module.exports = cartsService, ordersService, productsService, usersService;
+module.exports = { cartsService, ordersService, productsService, usersService };
