2cf8974689549bf1b5d1e7c3cce9cfbd8246547e
simontitk <simontitk@gmail.com>
2024-04-28 15:10:32 +0200

    add CORS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require("cors")
 const path = require('path');
 const createError = require('http-errors');
 
@@ -13,6 +14,7 @@ const categoriesRouter = require('./routes/categoriesRouter.js')
 
 const app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
